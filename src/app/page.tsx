caf332ad15e5e23bbee9d7fdc470b393e14ad792
w4t33r <vk.educate@protonmail.ch>
2024-04-05 13:50:11 -0700

    init workflow, clean project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import styles from './page.module.css';
 
 export default function Home() {
-  const im = 'drake'
+  const im = 'drake';
   return (
     <main className={styles.main}>
       <h1>Home</h1>
