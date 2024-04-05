de0e4d52cca1f1a001c8e5bb098f03c79b2fb651
w4t33r <vk.educate@protonmail.ch>
2024-04-05 15:26:41 -0700

    created home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import styles from './page.module.css';
 export default function Home() {
   const im = 'drake';
   return (
-    <main className={styles.main}>
-      <h1>Home</h1>
+    <main className={styles.container}>
+      <p>Main</p>
     </main>
   );
 }
