8d2bcbf0906962cf602a625364ccba4fb4a9338a
w4t33r <vk.educate@protonmail.ch>
2024-04-06 06:13:32 -0700

    created navbar component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,26 @@
+import styles from './navbar.module.css';
+import Image from 'next/image';
+
 const Navbar = () => {
   return (
-    <div>
-      <h2>Navbar</h2>
-    </div>
+    <header className={styles.navbar}>
+      <div className={styles.logo}>logos</div>
+      <nav className={styles.links}>
+        <span>Home</span>
+        <span>Gifts</span>
+        <span>About us</span>
+      </nav>
+      <div className={styles.links}>
+        <Image src={'./header-icons/search.svg'} width={32} height={32} alt={'searching-icon'} />
+        <Image
+          src={'./header-icons/shopping-cart.svg'}
+          width={32}
+          height={32}
+          alt={'searching-icon'}
+        />
+        <Image src={'./header-icons/user.svg'} width={32} height={32} alt={'searching-icon'} />
+      </div>
+    </header>
   );
 };
 
