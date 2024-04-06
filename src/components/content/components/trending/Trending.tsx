0d4035e11320694d74d9ef66dcd937c827e89ccf
w4t33r <vk.educate@protonmail.ch>
2024-04-06 13:10:45 -0700

    created trending component

--- src/components/content/components/trending/Trending.tsx
+++ src/components/content/components/trending/Trending.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import styles from './trending.module.css';
 import Title from '../sidebar/title/Title';
+import TrendingCard from '../trending/components/trendCards/TrendingCard';
+
 const Trending = () => {
   return (
     <div className={styles.container}>
-      <Title offer={'Trending'} />
+      <div className={styles.title}>
+        <Title offer={'Trending'} />
+      </div>
+      <div className={styles.trending}>
+        <TrendingCard title={'Gourmet chocolate box'} price={250} />
+        <TrendingCard title={'Gourmet chocolate box'} price={250} />
+        <TrendingCard title={'Gourmet chocolate box'} price={250} />
+        <TrendingCard title={'Gourmet chocolate box'} price={250} />
+        <TrendingCard title={'Gourmet chocolate box'} price={250} />
+        <TrendingCard title={'Gourmet chocolate box'} price={250} />
+      </div>
+      <div className={styles.title}>
+        <Title offer={'Trending'} />
+      </div>
+      <div className={styles.trending}>
+        <TrendingCard title={'f'} price={20} className={styles.customCard} hideContent={true} />
+        <TrendingCard
+          title={'f'}
+          price={20}
+          className={styles.customCard} // Вот здесь
+          hideContent={true}
+        />
+      </div>
     </div>
   );
 };
