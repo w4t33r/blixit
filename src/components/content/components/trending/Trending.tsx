0ec30762a0e33fadbfd93b3780c9620158679bae
w4t33r <vk.educate@protonmail.ch>
2024-04-06 10:32:56 -0700

    created Trending component

---
+++ src/components/content/components/trending/Trending.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './trending.module.css';
+import Title from '../sidebar/title/Title'
+const Trending = () => {
+  return (
+    <div className={styles.container}>
+      <Title offer={'Trending'}/>
+    </div>
+  );
+};
+
+export default Trending;
