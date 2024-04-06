066d8d07907f44a2e6a137795007b81569a49132
w4t33r <vk.educate@protonmail.ch>
2024-04-06 10:33:07 -0700

    created Trending component

--- src/components/content/components/trending/Trending.tsx
+++ src/components/content/components/trending/Trending.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './trending.module.css';
-import Title from '../sidebar/title/Title'
+import Title from '../sidebar/title/Title';
 const Trending = () => {
   return (
     <div className={styles.container}>
-      <Title offer={'Trending'}/>
+      <Title offer={'Trending'} />
     </div>
   );
 };
