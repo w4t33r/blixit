b13dc47f5a0f6be3847814872464a7e4e1aa220e
w4t33r <vk.educate@protonmail.ch>
2024-04-06 10:34:35 -0700

    import category, content into home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import styles from './page.module.css';
 import { Hero } from '@/components';
+import Category from '@/components/category/Category';
+import Content from '@/components/content/Content';
 
 export default function Home() {
   return (
     <main className={styles.container}>
       <Hero />
+      <Category />
+      <Content />
     </main>
   );
 }
